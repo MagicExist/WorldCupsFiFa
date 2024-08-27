bc386438f6ba4953589002e755c07703655e90b8
MagicExist <johhanparra1@gmail.com>
2024-08-27 18:01:00 -0500

    IChampionShipDbRepository Added

---
+++ API.Domain/Repository/IChampionShipDbRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Repository
+{
+    internal interface IChampionShipDbRepository
+    {
+        Task<IEnumerable<Groups>> GetGroupsByChampionShipAsync(int championShipId);
+    }
+}
