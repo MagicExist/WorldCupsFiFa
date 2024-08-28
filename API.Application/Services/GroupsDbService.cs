6b882a098963fff3b250b09027bf2d8301282d3e
MagicExist <johhanparra1@gmail.com>
2024-08-27 19:09:56 -0500

    GroupDbService Implemented

---
+++ API.Application/Services/GroupsDbService.cs
@@ -0,0 +1,21 @@
+﻿
+using API.Application.DTOs;
+using API.Domain.Repository;
+
+namespace API.Application.Services
+{
+    public class GroupsDbService
+    {
+        private readonly IGroupsDbRepository _groupsDbRepository;
+        public GroupsDbService(IGroupsDbRepository groupsDbRepository) 
+        {
+            _groupsDbRepository = groupsDbRepository;
+        }
+
+        public async Task<IEnumerable<GetGroupsByChampionShipDTO>> GetGroupsByChampionShip(int championShipId)
+        {
+            var dataResult = await _groupsDbRepository.GetGroupsByChampionShipAsync<GetGroupsByChampionShipDTO>(championShipId);
+            return dataResult;
+        }
+    }
+}
